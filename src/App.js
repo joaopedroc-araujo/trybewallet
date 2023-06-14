96640b72d1b1e86365c68cff8610469114c29f2b
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-06-14 20:56:21 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Header from './components/Header';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
@@ -9,7 +10,16 @@ function App() {
       <h1>Hello, TrybeWallet!</h1>
       <Switch>
         <Route path="/" exact component={ Login } />
-        <Route path="/carteira" exact component={ Wallet } />
+        <Route
+          path="/carteira"
+          exact
+          render={ () => (
+            <>
+              <Header />
+              <Wallet />
+            </>
+          ) }
+        />
       </Switch>
     </div>
   );
